f2ee7559ea93bd53dccd18a942b2c30d1e300e97
Tuğcan <tugcankoparan10@gmail.com>
2018-09-13 10:41:54 +0300

    Spring-Hibernate Configuration Example
    Spring-Hibernate Configuration Example

---
+++ src/com/tugcankoparan/EmployeeDao.java
@@ -0,0 +1,21 @@
+package com.tugcankoparan;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+
+public class EmployeeDao {
+HibernateTemplate template;
+public void setTemplate(HibernateTemplate template) {
+	this.template = template;
+}
+
+public void saveEmployee(Employee e){
+	template.save(e);
+}
+
+public void updateEmployee(Employee e){
+	template.update(e);
+}
+
+public void deleteEmployee(Employee e){
+	template.delete(e);
+}
+}
---
+++ src/com/tugcankoparan/InsertTest.java
@@ -0,0 +1,24 @@
+package com.tugcankoparan;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class InsertTest {
+public static void main(String[] args) {
+	
+	Resource r=new ClassPathResource("applicationContext.xml");
+	BeanFactory factory=new XmlBeanFactory(r);
+	
+	EmployeeDao dao=(EmployeeDao)factory.getBean("d");
+	
+	Employee e=new Employee();
+	e.setId(147);
+	e.setName("tugcan");
+	e.setSalary(70000);
+	
+	//dao.saveEmployee(e);
+	dao.updateEmployee(e);
+}
+}
